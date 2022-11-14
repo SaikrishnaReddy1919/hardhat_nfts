cec88b21dfa0361072917a04f161dde3b0a58b66
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2022-11-14 08:36:30 +0530

    basic nft, deploy, and tests

---
+++ utils/verify.js
@@ -0,0 +1,20 @@
+async function verify(contractAddress, args) {
+    //args -> if contract has any args in constructor
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified.")
+        } else {
+            console.log("Error while verifying : ", e)
+        }
+    }
+}
+
+module.exports = {
+    verify,
+}
