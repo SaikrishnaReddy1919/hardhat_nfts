60ad384f06a64b77d51d3ace96c635c04449cfe9
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2022-11-15 11:36:46 +0530

    dynamic nft full on chain contract

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -1,5 +1,6 @@
 //SPDX-License-Identifier: MIT
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "base64-sol/base64.sol";
 pragma solidity ^0.8.7;
 
@@ -9,24 +10,74 @@ contract DynamicSvgNft is ERC721 {
     //some logic to say show x image or show y image
 
     uint256 private s_tokenCounter = 0;
-    string private i_loaImageURI;
+    string private i_lowImageURI;
     string private i_highImageURI;
-    string private constant bbase64EncodedSvgPrefix = "data:image/svg+xml;base64,";
+    AggregatorV3Interface internal immutable i_priceFeed;
+    mapping(uint256 => int256) public s_tokenIdToHighValues;
 
-    constructor(string memory lowSvg, string memory highSvg) ERC721("DynamicSvgNft", "DSN") {
+    event CreatedNFT(uint256 indexed tokenId, int256 highValue);
+
+    constructor(
+        string memory lowSvg,
+        string memory highSvg,
+        address priceFeedAddress
+    ) ERC721("DynamicSvgNft", "DSN") {
         s_tokenCounter = 0;
+        i_lowImageURI = svgToImageURI(lowSvg);
+        i_highImageURI = svgToImageURI(highSvg);
+        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
     function svgToImageURI(string memory svg) public pure returns (string memory) {
         // takes in the svg as input
         // converts svg to base64 string
         // apends it the prefix above using which we can see the fetch.
+
+        // example:
+        // '<svg width="500" height="500" viewBox="0 0 285 350" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="black" d="M150,0,L75,200,L225,200,Z"></path></svg>'
+        // would return ""
+        //concatenate string
+        string memory baseURL = "data:image/svg+xml;base64,";
         string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
-        return string(abi.encodePacked(bbase64EncodedSvgPrefix, svgBase64Encoded));
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
     }
 
-    function mintNft() public {
-        _safeMint(msg.sender, s_tokenCounter);
+    function mintNft(int256 highValue) public {
+        s_tokenIdToHighValues[s_tokenCounter] = highValue;
         s_tokenCounter = s_tokenCounter + 1;
+        _safeMint(msg.sender, s_tokenCounter);
+        emit CreatedNFT(s_tokenCounter, highValue);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json:base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "URI query for nonexistent token");
+
+        (, int256 price, , , ) = i_priceFeed.latestRoundData();
+        string memory imageURI = i_lowImageURI;
+        if (price >= s_tokenIdToHighValues[tokenId]) {
+            imageURI = i_highImageURI;
+        }
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(), // You can add whatever name here
+                                '", "description":"An NFT that changes based on the Chainlink Feed", ',
+                                '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
     }
 }
